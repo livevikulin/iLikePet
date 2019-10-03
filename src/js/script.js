2b918d29f02368e1cc98bef718953ffe31051ce6
aheadweb <xalinanton@mail.ru>
2019-10-03 16:12:01 +0700

    + store adaptive template & catalog level one add template desctop

--- src/js/script.js
+++ src/js/script.js
@@ -151,6 +151,7 @@ $(document).ready(function() {
     //Фильтр
     $('.filter-box__item').click(function() {
 		$(this).next().slideToggle();
+		$(this).parent().toggleClass('filter-box_closed');
 		$(this).children('.fas').toggleClass('rotate180');
 	});
 
@@ -188,9 +189,31 @@ $(document).ready(function() {
 
 	//store page
 		// store-slider module
-		$('.sllider-brands').slick({
+		let sliderStore = $('.sllider-brands');
+		sliderStore.slick({
 			arrows: false,
+			variableWidth: true,
 		});
+		
+		$(".slider-back").on('click',function(){
+			sliderStore.slick("slickPrev")
+		})
+		$(".slider-next").on('click',function(){
+			sliderStore.slick("slickNext")
+		})
+
+		$(".filter-box__show-all").on('click',function(e){
+			e.preventDefault();
+			//filter-box__show-all_closed
+			let msg  = $(this).hasClass('filter-box__show-all_closed') ? msg = 'Скыть' : 'Показать еще';
+			$(this).children(':first').text(msg);
+			$(this).prev().find('li').each(function(i,item){
+				if(i > 3 ) {
+					$(".filter-box__show-all").hasClass('filter-box__show-all_closed') ? $(item).show() : $(item).hide();
+				}
+			})
+			$(this).toggleClass('filter-box__show-all_closed');
+		})
 	//store page end
 
 
